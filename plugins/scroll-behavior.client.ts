958fcd569281f7f0f21c8b303608ea019af25dd3
Danilo A. Silva <nilodna@gmail>
2023-11-10 09:22:36 -0300

    uploading new structure

---
+++ plugins/scroll-behavior.client.ts
@@ -0,0 +1,22 @@
+export default async function() {
+    if (typeof window !== "undefined") {
+        const router = useRouter()
+        
+        router.afterEach(async (to, from) => {
+            if (to.hash) {
+                const targetElement = document.querySelector(to.hash)
+
+                if (targetElement) {
+                    await targetElement.scrollIntoView({
+                        behavior: "smooth",
+                        block: "start",
+                        inline: "nearest"
+                    })
+                    return
+                }
+            }
+
+            window.scrollTo({ top: 0, behavior: "smooth"})
+        })
+    }
+}
