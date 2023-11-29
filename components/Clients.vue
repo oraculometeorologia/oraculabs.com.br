09a1bda04cc97417a5e9a3b12be32bf06c04a342
leomiyata <takashi.oraculo.me@gmail.com>
2023-11-29 18:00:09 -0300

    Made the website mobile-friendly

--- components/Clients.vue
+++ components/Clients.vue
@@ -1,56 +1,50 @@
 <template>
   <section id="clientes">
-      <div class="container p-5 text-dark text-center">
-        <h2 class="fs-1 pt-5 pb-5">Clientes | Parceiros</h2>
-        <div class="row mb-5">
-            <div v-for="value in clients" class="col-3 p-3">
-                <img :src="'/images/clients/logos/' + value.imgUrl" alt="" class="w-5 client-logo">
-            </div>
+    <div class="container py-5 text-dark text-center">
+      <h2 class="fs-1 py-4 py-md-5">Clientes | Parceiros</h2>
+      <div class="row mb-5">
+        <div v-for="value in clients" class="col-sm-6 col-md-3">
+          <img :src="'/images/clients/logos/' + value.imgUrl" alt="" class="w-5 client-logo">
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 </template>
 
 <script setup>
-
 const clients = [
-    {
-        imgUrl: 'logo-chilli-beans.webp',
-        alt: 'Chilli Beans Logo'
-    },
-    {
-        imgUrl: 'logo-fapesp.webp'
-    },
-    {
-        imgUrl: 'logo-lots-group-scania.webp'
-    },
-    {
-        imgUrl: 'logo-omega-energia.webp'
-    },
-    {
-        imgUrl: 'logo-sebrae.webp'
-    },
-    {
-        imgUrl: 'logo-startup-farm.webp'
-    },
-    {
-        imgUrl: 'logo-uniggel-sementes.webp'
-    },
-
+  {
+    imgUrl: 'logo-chilli-beans.webp',
+    alt: 'Chilli Beans Logo'
+  },
+  {
+    imgUrl: 'logo-fapesp.webp'
+  },
+  {
+    imgUrl: 'logo-lots-group-scania.webp'
+  },
+  {
+    imgUrl: 'logo-omega-energia.webp'
+  },
+  {
+    imgUrl: 'logo-sebrae.webp'
+  },
+  {
+    imgUrl: 'logo-startup-farm.webp'
+  },
+  {
+    imgUrl: 'logo-uniggel-sementes.webp'
+  },
 ]
-
-
 </script>
 
 <style >
 .client-logo {
-    transform: scale(0.9);
-    cursor: pointer;
-    transition: all 0.3s ease;
+  transform: scale(0.9);
+  transition: all 0.3s ease;
 }
 
 .client-logo:hover {
-    transform: scale(0.99);
+  transform: scale(0.99);
 }
-
 </style>
--- components/ProdutosCards.vue
+++ components/ProdutosCards.vue
@@ -1,46 +1,44 @@
 <template>
-    <section class="p-5" id="produtos">
-      <div class="container">
-        <div class="p-5 text-center">
-          <h2 class="fs-1">Principais produtos e serviços</h2>
-        </div>
-        <div class="row text-center">
-          <div class="col-lg card m-3 p-5 hover-effect-light-gray">
-            <div class="pb-2">
-              <i class="bi bi-cloud-arrow-up-fill fs-1" />
-            </div>
-            <h3 class="card-title h3">Banco de dados e infraestrutura cloud</h3>
+  <section class="py-5" id="produtos">
+    <div class="container text-center">
+      <h2 class="fs-1 py-4 py-md-5">Principais produtos e serviços</h2>
+
+      <div class="row gx-4">
+        <div class="col-sm-12 col-md-6 mb-4">
+          <div class="card h-100 p-4 p-md-5">
+            <i class="bi bi-cloud-arrow-up-fill fs-1 pb-2" />
+            <h3>Banco de dados e infraestrutura cloud</h3>
             <p>Organize a infraestrutura de armazenamento de dados e de acesso via nuvem.</p>
           </div>
-          <div class="col-lg card m-3 p-5 hover-effect-light-gray">
-            <div class="pb-2">
-              <i class="bi bi-database fs-1" />
-            </div>
-            <h3 class="card-title h3">Automatização de rotinas e criação de APIs</h3>
-            <p class="card-text">Automatize todo o processo de download, tratamento e disponibilização dos dados.</p>
+        </div>
+        <div class="col-sm-12 col-md-6 mb-4">
+          <div class="card h-100 p-4 p-md-5">
+            <i class="bi bi-database fs-1 pb-2" />
+            <h3>Automatização de rotinas e criação de APIs</h3>
+            <p>Automatize todo o processo de download, tratamento e disponibilização dos dados.</p>
           </div>
         </div>
-
-        <div class="row text-center">
-          <div class="col-lg card m-3 p-5 hover-effect-light-gray">
-            <div class="pb-2">
-              <i class="bi bi-graph-up-arrow fs-1" />
-            </div>
-            <h3 class="card-title h3">Análises estatísticas e inteligência artificial</h3>
+        <div class="col-sm-12 col-md-6 mb-4">
+          <div class="card h-100 p-4 p-md-5">
+            <i class="bi bi-graph-up-arrow fs-1 pb-2" />
+            <h3>Análises estatísticas e inteligência artificial</h3>
             <p>Tenha dados com estatística ou IA de acordo com as necessidades do seu negócio.</p>
           </div>
-          <div class="col-lg card m-3 p-5 hover-effect-light-gray">
-            <div class="pb-2">
-              <i class="bi bi-laptop fs-1" />
-            </div>
-            <h3 class="card-title h3">Interfaces web estáticas ou dinâmicas</h3>
+        </div>
+        <div class="col-sm-12 col-md-6 mb-4">
+          <div class="card h-100 p-4 p-md-5">
+            <i class="bi bi-laptop fs-1 pb-2" />
+            <h3>Interfaces web estáticas ou dinâmicas</h3>
             <p>Visualize as informações de forma customizada, via dashboards e relatórios.</p>
           </div>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 </template>
 
 <script setup></script>
 
-<style scoped></style>
+<style scoped>
+/* */
+</style>
