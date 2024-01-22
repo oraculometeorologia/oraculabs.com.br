dde830a2a29eb02ab3d5586ff7f5374d8412c2b4
leomiyata <takashi.oraculo.me@gmail.com>
2024-01-22 17:44:18 -0300

    Fixed GA4 tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,12 +9,6 @@ export default defineNuxtConfig({
   ],
   script: [
     {
-      async: true,
-      src: 'https://www.googletagmanager.com/gtag/js?id=G-37LSN28ET2'
-    }, {
-      children: "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-37LSN28ET2');"
-    },
-    {
       src: "@/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
       type: "text/javascript"
     }
@@ -24,17 +18,24 @@ export default defineNuxtConfig({
       link: [
         {
           href: "https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&display=swap", rel: "stylesheet"
+        },
+        // Google Analytics
+        {
+          async: true,
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-37LSN28ET2'
+        }, {
+          children: "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-37LSN28ET2');"
         }
-      ],
-      // script: [
-      //   // Bootstrap
-      //   {
-      //     src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js',
-      //     body: true,
-      //     integrity: "sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe",
-      //     crossorigin: "anonymous"
-      //   },
-      // ]
+        // script: [
+        //   // Bootstrap
+        //   {
+        //     src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js',
+        //     body: true,
+        //     integrity: "sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe",
+        //     crossorigin: "anonymous"
+        //   },
+        // ]
+      ]
     }
   }
 })
